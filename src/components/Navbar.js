8016ab03830ef4ea586382dc3d225fcd5c3d5a62
Minakoss <nontaspapastefanis@gmail.com>
2024-09-24 23:36:37 +0300

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+      </div>
+
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
         <li>
-          <Link to="/">Αρχική</Link>
+          <Link to="/" onClick={() => setIsOpen(false)}>
+            Αρχική
+          </Link>
         </li>
         <li>
-          <Link to="/about">Σχετικά με εμάς</Link>
+          <Link to="/about" onClick={() => setIsOpen(false)}>
+            Σχετικά με εμάς
+          </Link>
         </li>
         <li>
-          <Link to="/adoption">Βρείτε κατοικίδιο</Link>
+          <Link to="/adoption" onClick={() => setIsOpen(false)}>
+            Βρείτε κατοικίδιο
+          </Link>
         </li>
         <li>
-          <Link to="/actions">Δράσεις</Link>
+          <Link to="/actions" onClick={() => setIsOpen(false)}>
+            Δράσεις
+          </Link>
         </li>
         <li>
-          <Link to="/filozoikes">Φιλοζωικές</Link>
+          <Link to="/filozoikes" onClick={() => setIsOpen(false)}>
+            Φιλοζωικές
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Επικοινωνία</Link>
+          <Link to="/contact" onClick={() => setIsOpen(false)}>
+            Επικοινωνία
+          </Link>
         </li>
       </ul>
     </nav>
